f82f2b725778d6a92fc72beb5a4bf5480eaf03cd
Jess Peters <jlpeters94@gmail.com>
2020-11-23 16:35:34 +0800

    Fixed raw data input message

--- home/bikeshare.py
+++ home/bikeshare.py
@@ -146,13 +146,13 @@ def user_stats(df):
 def display_data(df):
     start = 0
     stop = 5
-    display = input("\nDo you want to see the raw data? Type 'yes' or 'no'\n")
+    display = input("\nDo you want to see the raw data? Enter yes or no\n")
     while display.lower() == 'yes':
         show = df.iloc[start:stop]
         print(show)
         start += 5
         stop += 5
-        display = input("\nDo you want to see more raw data? Type 'yes' or 'no'\n")
+        display = input("\nDo you want to see more raw data? Enter yes or no\n")
     
 def main():
     while True:
